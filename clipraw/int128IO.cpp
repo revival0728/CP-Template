cd24d819069ef9d37703dc740c87384c778d95cc
revival0728 <revival0728@gmail.com>
2021-12-20 13:28:32 +0800

    update int128IO

--- clipraw/int128IO.cpp
+++ clipraw/int128IO.cpp
@@ -1,12 +1,16 @@
 namespace int128IO {
 	istream& operator>>(istream& is, __int128& i) {
 		string s; is>>s; i = 0;
-		for(auto& c : s) i=i*10+(c-'0');
+		auto c=s.begin(); c+=(s[0]=='-');
+		for(; c!=s.end(); ++c) i=i*10+(*c-'0');
+		if(s[0]=='-') i=-i;
 		return is;
 	}
 	ostream& operator<<(ostream& os, __int128 i) {
-		string s; while(i) s+=('0'+i%10), i/=10;
-		for(auto it=s.rbegin();it!=s.rend();++it) os<<*it;
+		string s; bool neg=false; if(i<0) neg=true, i=-i;
+		while(i) s+=('0'+i%10), i/=10;
+		if(neg) os<<'-';
+		for(auto c=s.rbegin();c!=s.rend();++c) os<<*c;
 		return os;
 	}
 }
