3b786376e3963e71dc23bac77799a8b12d8f3826
revival0728 <revival0728@gmail.com>
2023-03-29 20:43:06 +0800

    update BufferIO

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -10,10 +10,22 @@ mod template;
 use std::io;
 use std::collections::HashMap;
 use std::collections::HashSet;
+use crate::template::buffer_io::BufferIO;
 
 macro_rules! to_usize { ($num: ident) => { usize::try_from($num).unwrap() } }
 
+fn main() {
+    let mut IO = BufferIO::new(true);
 
+    let a: i32 = IO.read_item();
+    let b: Vec<i32> = IO.read_vec();
+    let [c, d]: [i32] = IO.read_vec()[..] else { todo!() };
 
-fn main() {}
+    fprintln!(IO, "a = {}, b = {}, c = {}", a, c, d);
+    // println!("a = {}, c = {}, d = {}", a, c, d);
+    IO.println(&b[..]);
+    IO.println(&[a, c, d]);
+    IO.println(&["OK"]);
+    IO.write("OK\n");
+}
 
