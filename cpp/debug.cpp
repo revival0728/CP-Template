c44265457ba28ea7769b0457d6c75e17b79a789b
Hsing Fu <69998786+revival0728@users.noreply.github.com>
2024-02-17 20:03:19 +0800

    Update debug.cpp

--- cpp/debug.cpp
+++ cpp/debug.cpp
@@ -4,6 +4,8 @@ using namespace std;
 void debug() {}
 template<class T> void debug(T var) { cerr << var; }
 template<class T, class ...P> void debug(T var, P ...t) { cerr << var << ", "; debug(t...); }
+template<class T> void org(T l, T r) { while(l != r) cerr << *l++ << ' '; }
 #define de(...) { cerr << "[Line: " << __LINE__ << "][" << #__VA_ARGS__ << "] -> [", debug(__VA_ARGS__), cerr << "]\n"; }
+#define orange(...) { cerr << "[Line: " << __LINE__ << "][" << #__VA_ARGS__ << "] -> [", org(__VA_ARGS__), cerr << "]\n"; }
 
 int main() {}
