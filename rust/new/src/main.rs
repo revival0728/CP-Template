c522b1ec6b296516a607e7cdd350ccf204c3628e
revival0728 <revival0728@gmail.com>
2024-03-19 16:21:08 +0800

    update cpio

--- rust/new/src/main.rs
+++ rust/new/src/main.rs
@@ -1,5 +1,17 @@
 mod modular;
-mod io;
+mod cpio;
+
+use cpio::cpio::*;
 
 fn main() {
+  let s = read_str!().unwrap();
+  let t = read_str!().unwrap();
+  let a = read!(usize).unwrap();
+  let (b, c) = read!(i32, i32).unwrap();
+  let v = read_vec!(i32).unwrap();
+
+  println!("{}", s);
+  println!("{}", t);
+  println!("{} {} {}", a, b, c);
+  println!("{}", vec_join_space(&v));
 }
